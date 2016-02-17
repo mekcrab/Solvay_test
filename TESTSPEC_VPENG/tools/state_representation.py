a799fcc9e050737321243b6803a7e2669949ce6f
pengtianyue <pengtianyue@hotmail.com>
2016-02-17 14:13:17 -0500

    old logic was completely wrong.....still working on it

--- TESTSPEC_VPENG/tools/state_representation.py
+++ TESTSPEC_VPENG/tools/state_representation.py
@@ -1,57 +1,64 @@
 __author__ = 'vpeng'
 
-from puml_dict_parser import GetDict
+from puml_dict_parser import GetDict, GetList
 from TESTSPEC_EKOPACHE.tools import StateAlgo
 
 
+def recur(in_state):
+    StateAlgo.StateAlgorithm().add_state(state_object = in_state)
+    Dest = GetDict().source_destination()[in_state] #List
+    Action = []
+
+    if in_state in GetList().AState:
+        Action = GetDict().state_action()[in_state] #List
+
+    StateAlgo.State(state_id = in_state, actions = Action).activate()
+
+    connection = []
+    for item in range(0,len(Dest)):
+        connection.append((in_state, Dest[item]))
+        if connection[item] in GetDict().states_transition():
+            Transition = GetDict().states_transition()[connection[item]] #List
+            for n in range(0, len(Transition)):
+                StateAlgo.StateAlgorithm().add_trasition(transition = Transition[n],
+                                                        current_state = in_state, next_state = Dest[item])
+
+
+
+StateAlgo.StateAlgorithm().deactivate_all()
+
+recur('*') #Start
+
+if StateAlgo.StateAlgorithm().evaluate_transitions():
+    StateAlgo.StateAlgorithm().change_state(done_state = '*', new_state = Dest, permissive = True)
+
+
+
 '''with [*], find it's destination(First Step) in source'''
-start = GetDict.source_destination()['*'] #Get the first State after [*]
 
-in_state = start  #List
-dest_state = GetDict.source_destination()[start]  # List
 
-for x in range(0,len(in_state))
-    for y in range(0,len(dest_state)):
-        while dest_state[y] != '[*]':
-            '''recurring start'''
+start = '*'
 
-            '''add_state'''
-            StateAlgo.StateAlgorithm.add_state(state_object = in_state[x])
+input = start
+dest = GetDict.source_destination()[input] #Get the State (List) after [*]
+start_link = (input,dest)
 
-            '''add_action and evaluate(if any)'''
-            if in_state[x] in GetDict.state_action():
-                action = GetDict.state_action()[in_state[x]] # List
-                for n in range(0,len(action)):
-                    StateAlgo.State.add_action(action_obj = action[n])
-                    StateAlgo.Action.evaluate(action[n])
+StateAlgo.State(state_id = input).activate()
 
-            '''find dest_state'''
-            dest_states = GetDict.source_destination()[in_state[x]] # List
+if start_link in GetDict.states_transition():
+    start_transitions = GetDict.states_transition()[start_link]
+    StateAlgo.StateAlgorithm.evaluate_transitions()
+    StateAlgo.StateAlgorithm.change_state(done_state = input, new_state = dest, check_permissive = True)
+else:
+    StateAlgo.StateAlgorithm.change_state(done_state = input, new_state = dest, check_permissive = True)
 
-            '''add_transition (if any)'''
-            for z in dest_states:
-                connection = (in_state[x], dest_states[z])
+all_dest = GetList().DestState() #List
 
-                if connection in GetDict.states_transition():
-                    transition = GetDict.source_transition()[in_state[x]] #List
-                    for m in range(0,len(transition)):
-                        StateAlgo.StateAlgorithm.add_trasition(transition = transition[m], current_state = in_state[x],
-                                                               next_state = dest_states[z])
-                        '''evaluate_transition'''
-                        StateAlgo.StateAlgorithm.evaluate_transitions()
+for item in range(0,len(all_dest)):
+    if StateAlgo.State(state_id = all_dest[item]).is_active():
 
-                    '''change_state'''
-                    permissive = StateAlgo.Transition.check_condition()
-                    StateAlgo.StateAlgorithm.change_state(done_state = in_state[x], new_state = dest_states[z],
-                                                          check_permissive = permissive)
+        #TODO: startloop
 
-                else:
-                    '''change state'''
-                    StateAlgo.StateAlgorithm.change_state(done_state = in_state[x], new_state = dest_states[z],
-                                                          check_permissive = True)
-
-                '''recurring...'''#TODO:......
-                new_state = dest_state
 
 
 StateAlgo.StateAlgorithm.deactivate_all()
