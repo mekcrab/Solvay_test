14160c72197dd91686b42a6cd33cab3886886880
mekcrab <erik@ek-linux.net>
2016-02-22 18:10:25 -0500

    added prettyprint utility to config module

--- tools/config.py
+++ tools/config.py
@@ -5,6 +5,7 @@ Module contains configuration definitions for plantUML tools
 __author__ = "erik"
 
 import os
+import pprint
 
 # current path of tools folder
 tools_path = os.getcwd()
@@ -19,20 +20,30 @@ tests_path = os.path.join(tools_path, '..', 'tests_out')
 java_path = 'java'
 
 # path to plantUML pre-compiled jar file
-plantUML_jar = os.path.join('/','home', 'erik', '.PyCharm50',
+plantUML_jar = os.path.join('/','home', 'erik', '.PyCharm40',
                             'config', 'plugins', 'plantuml4idea',
                             'lib', 'plantuml.jar')
 
-class sys_utils():
+class sys_utils:
 
     '''Aggregated system utilities for convenience'''
 
-    def set_pp_on(self):
-        '''Sets pretty printing as default terminal displayhook'''
-        pass
+    default_stdout = os.sys.stdout
+    default_displayhook = os.sys.displayhook
 
-    def set_pp_off(self):
+    @staticmethod
+    def pp_hook(value):
+        if value != None:
+            pprint.pprint(value)
+
+    @staticmethod
+    def set_pp_on():
+        '''Sets pretty printing as default terminal displayhook'''
+        setattr(os.sys, 'displayhook', sys_utils.pp_hook)
+
+    @staticmethod
+    def set_pp_off():
         '''Reverts to default terminal displayhook'''
-        pass
+        setattr(os.sys, 'displayhook', sys_utils.default_displayhook)
 
 
