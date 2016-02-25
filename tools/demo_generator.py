0ce1f887ebed59fccfacdd4c5e63d7fde3228271
pengtianyue <pengtianyue@hotmail.com>
2016-02-25 15:09:31 -0500

    Demo_generator for substates and single transition attribute complete (sample test: Demo_1.0.puml).

--- tools/demo_generator.py
+++ tools/demo_generator.py
@@ -57,7 +57,7 @@ class Action():
         return self.complete
 
 class TranAttr():
-    def __init__(self, tran_attr, connection):
+    def __init__(self, tran_attr, connection): # input should a single tran_attr tuple (..,..,..)
         self.tran_attr = tran_attr
         tokens = split_attr(tran_attr)
         self.connection = connection
@@ -81,43 +81,43 @@ class TranAttr():
         elif '!' in self.condition and self.PV != self.target:
             self.reached = True
         else:
-            raise TypeError
+            print "Transition of %r FAIL" %(self.tran_attr)
 
         return self.reached
 
 
 def runsub(parent, diagram):
-    get_state = diagram.get_state(state_id = parent)
-    substates = get_state.substates
+    get_parent = diagram.get_state(state_id = parent)
+    parentdest = get_parent.destination
+    substates = get_parent.substates
     for substate in substates:
         sub = diagram.get_state(state_id = substate)
         subsource = sub.source
+        subdest = sub.destination
         if subsource == []:
             recur(substate, diagram, connection)
-        else:
-            continue
+        if subdest == []:
+            print "*Test for substate(s) in state: %r complete" %(parent.name)
+            transit(parent,parentdest,connection)
 
 
-
-#Start
-#in_state = '[*]'
-#State(name = in_state).activate()
 
 def recur(in_state, diagram, connection):
     get_in = diagram.get_state(state_id = in_state)
     in_name = get_in.name
+    substates = diagram.get_state(state_id = in_state).substates
     print "________Testing State::: %r ________" %(in_name)
-
+    num_substates = len(get_in.substates)
     destination = get_in.destination  # List of Object
     state_attr = get_in.attrs
-    print "substates number:", get_in.num_substates
+    print "substates number:", num_substates
 
     #FIXME: substate_num and substates objects empty in StateModel
     ''' Run substate (if any) or execute action'''
-    if int(get_in.num_substates) > 0:
-
+    if int(num_substates) > 0:
+        print "*Substate(s) found in state: %r. \nStart running substates below:" %(in_name)
         runsub(in_state, diagram)
-    elif int(get_in.num_substates) == 0:
+    elif int(num_substates) == 0:
         are_complete = list()
         for action in state_attr:
             a = Action(action, connection)
@@ -140,44 +140,47 @@ def transit(in_state, destination, connection):
     get_in = diagram.get_state(state_id = in_state)
     in_name = get_in.name
 
-    print "Transiting..."
+    print "Transiting......"
 
     for dest_state in destination:
         get_dest = diagram.get_state(state_id = dest_state)
         dest_name = get_dest.name
 
         '''Transition Attribute'''
-        tran_attr = diagram.get_transitions(source=in_name, dest=dest_name)
+        tran_list = diagram.get_transitions(source=in_name, dest=dest_name) # List of transition attrs
         #FIXME: Transition Attribute always empty, check StateModel.py
-        #print "in_state:", in_name, ", dest_state:", dest_name, "have transition:", tran_attr
+        for tran_attr in tran_list:
+            tran_attr = tran_attr.attrs
+            #print "in_state:", in_name, ", dest_state:", dest_name, "have transition:", tran_attr
 
-        reached = list()
+            reached = list()
 
-        for item in tran_attr:
+            for item in tran_attr:
+                print "Verifying:", item
 
-            if TranAttr(item, connection).verify():
-                reached.append(True)
-            else:
-                reached.append(False)
+                if TranAttr(item, connection).verify():
+                    reached.append(True)
+                else:
+                    reached.append(False)
 
-        if False not in reached:
+            if False not in reached:
 
-            #State(name = in_state).deactivate()
-            if dest_name != 'END' or []:
-                #State(name = dest_state).activate()
-                recur(dest_state, diagram, connection)
-            elif dest_name == 'END':
-                print "===========Test Complete=========="
+                #State(name = in_state).deactivate()
+                if dest_name != 'END' or []:
+                    #State(name = dest_state).activate()
+                    recur(dest_state, diagram, connection)
+                elif dest_name == 'END':
+                    print "===========Test Complete=========="
 
 
 if __name__ == "__main__":
     from ModelBuilder import StateModelBuilder
     import config, os, time
-    from plantUML_state_lexer import get_tokens_from_file
+    from PlantUML_Lexer import get_tokens_from_file
     connection = OPC_Connect()
     time.sleep(3)
 
-    input_path = os.path.join(config.specs_path, 'vpeng', 'Demo_0.0.puml')
+    input_path = os.path.join(config.specs_path, 'vpeng', 'Demo_1.0.puml')
 
     tkns = get_tokens_from_file(input_path)
 
