b20ea142a8ad8fc4ee2b9164058f39988edac98b
pengtianyue <pengtianyue@hotmail.com>
2016-03-28 16:44:16 -0400

    Built a very immature logic (for demo purpose) to test_admin_processor to set EM command while test of each test case start.

--- unit_test/test_admin_processor.py
+++ unit_test/test_admin_processor.py
@@ -3,33 +3,74 @@ __author__ = 'vpeng'
 import test_solver_processor
 import time
 from tools.TestAdmin import Test
-from tools import TestAdmin
 from tools.serverside.OPCclient import OPC_Connect, OPCdummy
 
-dlog = TestAdmin.dlog
+class RunEM():
 
-def S_EMC_PRESS_CND():
-    test_gen, diagram = test_solver_processor.S_EMC_PRESS_CND()
-    return test_admin_processor(test_gen, diagram)
+    def __init__(self):
+        pass
 
-def S_EMC_CHARGE():
-    test_gen, diagram = test_solver_processor.S_EMC_CHARGE()
-    return test_admin_processor(test_gen, diagram)
+    def S_EMC_PRESS_CND(self):
+        self.command_dict = {
+        'HOLD': 0,
+        'CLOSE_ALL': 1,
+        'START_VACUUM': 2,
+        'STOP_VACUUM': 3,
+        'PRESSURE_UP': 4,
+        'VENT': 5,
+        'DEAERATE': 6,
+    }
 
-def S_EMC_CHG_BLWDN():
-    test_gen, diagram = test_solver_processor.S_EMC_CHG_BLWDN()
-    return test_admin_processor(test_gen, diagram)
+        test_gen, diagram = test_solver_processor.S_EMC_PRESS_CND()
+        return self.test_admin_processor(test_gen, diagram)
+
+    def S_EMC_CHARGE(self):
+        self.command_dict = {
+        'HOLD': 0,
+        'CLOSE_ALL': 1,
+        'CHARGE': 2,
+        'RESET_TOTALS': 3,
+    }
+
+        test_gen, diagram = test_solver_processor.S_EMC_CHARGE()
+        return self.test_admin_processor(test_gen, diagram)
+
+    def S_EMC_CHG_BLWDN(self):
+        self.command_dict = {
+        'HOLD': 0,
+        'CLOSE_ALL': 1,
+        'CHARGE': 2,
+        'RESET_TOTALS': 3,
+    }
+
+        test_gen, diagram = test_solver_processor.S_EMC_CHG_BLWDN()
+        return self.test_admin_processor(test_gen, diagram)
+
+    def test_admin_processor(self, test_gen, diagram):
+
+        connection = OPC_Connect()
+        time.sleep(1)
+
+        from tools import TestAdmin
+
+        dlog = TestAdmin.dlog
+        logger = dlog.MakeChild('TestAdmin')
+        logger.debug("Start Testing Diagram::: %r", diagram.id)
+
+        for solved_path in test_gen.test_cases:
+            test_case = test_gen.test_cases[solved_path].diagram
+            logger.debug("Testing Solved Test Case: %r", solved_path)
+
+            # Set EM Command path and target
+            command_path = '/'.join([str(diagram.id).strip("'"),'A_COMMAND.CV'])
+            command_name = solved_path.split(' ')[1].split('-')[0]
+            command = self.command_dict[command_name]
+
+            # Start EM Command
+            connection.write(command_path, command)
+
+            Test(test_case= test_case, diagram = diagram, connection = connection).start()
 
 
-def test_admin_processor(test_gen, diagram):
-
-    connection = OPC_Connect()
-    time.sleep(1)
-
-    logger = dlog.MakeChild('TestAdmin')
-    logger.debug("Start Testing Diagram::: %r", diagram.id)
-
-    for solved_path in test_gen.test_cases:
-        test_case = test_gen.test_cases[solved_path].diagram
-        logger.debug("Testing Solved Test Case: %r", solved_path)
-        Test(test_case= test_case, diagram = diagram, connection = connection).start()
+if __name__ == "__main__":
+    RunEM().S_EMC_PRESS_CND()
