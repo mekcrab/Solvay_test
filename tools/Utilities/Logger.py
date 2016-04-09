c742a8208dc1752047e863c39b6bed831c17605e
mekcrab <mekcrab@gmail.com>
2016-04-09 06:53:41 -0400

    Logging updates - tests now can create their own log files

--- tools/Utilities/Logger.py
+++ tools/Utilities/Logger.py
@@ -8,7 +8,7 @@ Example calling:
 from Utilities.Logger import LogTools
 import config
 
-dbug_log = LogTools(config.log_path + 'StateModel.log', config.log_level, 'StateModel')
+dbug_log = LogTools(config.log_path + 'StateModel.log', 'StateModel', level=config.log_level)
 dbug_log.rootlog.warning('StateModel initialized')
 
 class MyClass(object):
@@ -37,102 +37,151 @@ import logging.handlers
 import tools.config as config
 from sys import stdout
 
+# logging output level dictionary, lowest --> highest levels.
+# Lower levels include all higher levels (ex. 'debug' gives everything.
+LEVELS = {'debug': logging.DEBUG,
+          'info': logging.INFO,
+          'warning': logging.WARNING,
+          'error': logging.ERROR,
+          'critical': logging.CRITICAL,}
+
+
+def get_log_level(level):
+    '''Converts logging level sting names to logging module's level constant'''
+    if level not in LEVELS.values():
+        return LEVELS.get(level, logging.NOTSET)  # defaults to NOTSET (the lowest level possible)
+    else:
+        return level
+
 
 class LogTools(object):
-    def __init__(self, log_filename='test.log', module="ModuleName",
-                        level=config.log_level,
-                        maxSize=1000000, maxCount=5,
-                        streamLevel=config.log_level):
-        '''Initialize Root Logger Instance'''
-        self.fileformat = logging.Formatter('%(asctime)s ::%(levelname)8s:: %(name)15s:: %(message)s')
+    def __init__(self,
+                 log_filename='test.log',
+                 module="ModuleName",
+                 level=config.log_level,
+                 maxSize=1000000,
+                 maxCount=5,
+                 streamLevel=None):
+
+        '''Initialize root logger instance and create initial handlers'''
+
+        # self.modulename is the string name of the log handled by this LogTools instance
+        self.modulename = module
+
+        # define log printing format
+        self.fileformat = logging.Formatter('%(asctime)s ::%(levelname)8s:: %(name)15s::\t %(message)s')
+
+        # create new log handler
         self.handler = logging.handlers.RotatingFileHandler(
                                                             os.path.join(config.log_path, log_filename),
                                                             maxBytes=maxSize,
                                                             backupCount=maxCount,
                                                             )
-        # initialize root logging instance
-        self.modulename = module
+        # initialize root logging instance, set logging options
         self.handler.setFormatter(self.fileformat)
-        self.handler.setLevel(logLevel(level))
-        self.rootlog = logging.getLogger(self.modulename)
-        self.rootlog.addHandler(self.handler)
-        if streamLevel:
-            self.streamformat = logging.Formatter('%(levelname)s:: %(name)s:: %(message)s')
-            self.shandler = logging.StreamHandler()
-            self.shandler.setLevel(logLevel(streamLevel))
-            self.shandler.setFormatter(self.streamformat)
-            self.rootlog.addHandler(self.shandler)
-            self.rootlog.propagate = 0
+        self.handler.setLevel(get_log_level(level))
 
-    def MakeChild(self, name=__name__, level_name='debug'):
-        if not level_name:
-            level_name = self.handler.level
+        # set the root log based on module name (log already created, needs to be fetched by python logging module)
+        self.rootlog = self.get_root_log()
+
+        # bind our handler specification to this log
+        self.rootlog.addHandler(self.handler)
+
+        # set initial level for this logs stream output - applies to console outputs or log pipes
+        if streamLevel is None:
+            streamLevel = level
+
+        self.streamformat = logging.Formatter('%(levelname)s:: %(name)s:: %(message)s')
+        self.shandler = logging.StreamHandler()
+        self.shandler.setLevel(get_log_level(streamLevel))
+        self.shandler.setFormatter(self.streamformat)
+        self.rootlog.addHandler(self.shandler)
+        self.rootlog.propagate = False
+
+    def get_root_log(self):
+        '''Returns a handle to the root log of this LogTools instance hierarchy'''
+        return logging.getLogger(self.modulename)
+
+    def MakeChild(self, name=None, level=None):
+        '''Makes a child logger of the logger specified by name'''
+        if name is None:
+            name = self.rootlog.name
+
+        # inherit logging level from rootlog RotatingFileHandler unspecified
+        if level is None:
+            level = self.handler.level
+        else:
+            level = get_log_level(level)
+
         childlog = self.rootlog.getChild(name)
-        childlog.setLevel(logLevel(level_name))
+        childlog.setLevel(level)
         return childlog
 
     def GetLogger(self, name, level_name='debug'):
+        '''
+        Wrapper method to combine logger outputs to the
+        logger referenced by name. Sets logging level if passed optional argument level_name
+        '''
         newlogger = logging.getLogger(name)
         newlogger.addHandler(self.handler)
         newlogger.addHandler(self.shandler)
-        newlogger.setLevel(logLevel(level_name))
-        newlogger.propagate = 0
+        newlogger.setLevel(get_log_level(level_name))
+        newlogger.propagate = False
         return newlogger
 
-    def Output2Stdout(self, level='info'):
-        '''Method changes log's output from rotating file handler to the standard out'''
-        self.rootlog.removeHandler(self.handler)
-        self.rootlog.removeHandler(self.shandler)
+    def Output2Stdout(self, level_name='info', stop_log_file=False):
+        '''
+        Method changes log's output from rotating file handler to the standard out
+        :param level_name: logging level to push to stdout
+        :param stop_log_file: flag to stop logging to original output file
+        '''
+        # create new handler for console output
         stdout_handler = logging.StreamHandler(stdout)
         stdout_handler.setFormatter(logging.Formatter('%(asctime)s :: %(name)s :: %(message)s'))
-        stdout_handler.setLevel(logLevel(level))
+        stdout_handler.setLevel(get_log_level(level_name))
+        # add handler to rootlog
         self.rootlog.addHandler(stdout_handler)
-
-
-def logLevel(level):
-    LEVELS = {'debug': logging.DEBUG,
-              'info': logging.INFO,
-              'warning': logging.WARNING,
-              'error': logging.ERROR,
-              'critical': logging.CRITICAL,
-              }
-
-    loglvl = LEVELS.get(level, logging.NOTSET)
-    return loglvl
+        # inform user of logging level change
+        self.rootlog.info("Logging output directed to console")
+        # disable original log file handler if flag set
+        if stop_log_file:
+            self.rootlog.warning("Logging output to file disabled, all output directed to console")
+            self.rootlog.removeHandler(self.handler)
+            self.rootlog.removeHandler(self.shandler)
 
 
 class AuxLog(object):
     '''
-    Helper class to log errors to a file for system troubleshooting and debugging
+    Helper class to log errors to a new file for (hopefully) easier troubleshooting and debugging
     '''
 
     def __init__(self, log_filename, level, module='Root'):
-        ''' Initializes a ErrorLog instance'''
-        LEVELS = {'debug': logging.DEBUG,
-                  'info': logging.INFO,
-                  'warning': logging.WARNING,
-                  'error': logging.ERROR,
-                  'critical': logging.CRITICAL,
-                  }
-
-        if len(level) > 1:
-            level_name = level
-            self.level = LEVELS.get(level_name, logging.NOTSET)
-        self.handler = logging.handlers.RotatingFileHandler(log_filename,
-                                                            maxBytes=10000,
-                                                            backupCount=5,
-                                                            )
-
+        ''' Initializes a new auxiliary log instance.'''
+        # create new log file handler
+        self.handler = logging.handlers.RotatingFileHandler(
+                                                    log_filename,
+                                                    maxBytes=10000,
+                                                    backupCount=5,
+                                                    )
+        # set handler options
+        if level not in LEVELS.values():
+            self.level = LEVELS.get(level, logging.NOTSET)
         self.handler.setLevel(self.level)
+
         self.formatter = logging.Formatter("%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s")
         self.handler.setFormatter(self.formatter)
-        # logging.basicConfig(filename=log_filename,level=level, format ='%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s' )
+
+        # bind the log defined by <module> to the new handler
         self.rootlogger = logging.getLogger(module)
         self.rootlogger.addHandler(self.handler)
-        self.rootlogger.debug('Logger Initialized')
-        pass
+        self.rootlogger.info('New auxillary log initialized.')
 
-    def SetLogger(self, module):
-        clog = logging.getLogger(module)
+    def SetLogger(self, log_name):
+        '''
+        Adds the output of log specified by string <log name> to the auxillary log
+        :param module:
+        :return: logger instance with added handler to output to AuxLog file
+        '''
+        clog = logging.getLogger(log_name)
         clog.addHandler(self.handler)
         return clog
--- tools/serverside/OPCclient.py
+++ tools/serverside/OPCclient.py
@@ -1,11 +1,22 @@
-'''Example module to handle OpenOPC connections
+'''
+Example module to handle OpenOPC connections
 
-Module must be run from windows python (C:\python.exe)
-OPC access is via OPC gateway, with OPC gateway service run as DeltaVAdmin
+Module must be run from windows python (C:\python.exe) on Cygwin installations.
+OPC access is via an OPC gateway, run OPC gateway service run as DeltaVAdmin to avoid DCOM
+permissions errors when interacting with a DeltaV system.
 
+Includes dummy OPC client to allow for testing if OPC connections are not available. Module will
+select real/dummy client based on import errors and bind the required class to the name OPC_connect
+
+Use this construct to allow for real/dummy client autoselect:
+from OPCclient import OPC_connect
 '''
 
 import time
+
+from tools.Utilities.Logger import LogTools
+dlog = LogTools('OPCclient.log', module='OPCclient')
+dlog.rootlog.info('OPCclient module initialized.')
 
 # aliased class names based on import sucess
 OPC_AVAILABLE = False
@@ -14,36 +25,49 @@ try:
     import Pyro.core
     OPC_AVAILABLE = True
 except ImportError:
-    print "No OpenOPC package found, revert to dummy client"
+    dlog.rootlog.error("No OpenOPC package found, reverting to dummy client.")
 
 
-class OPCconnect(object):
+class OPCclient(object):
     '''
     OPC read/write client connection
     '''
     def __init__(self, srv_name='OPC.DeltaV.1'):
+        self.server_name = srv_name
         self.client = self.connect_local(srv_name)
-        self.is_dummy = False
+        self.is_dummy = False  # marker to determine real client use at runtime
 
-    def connect_local(self, svr_name, host='localhost'):
+    @staticmethod
+    def connect_local(svr_name, host='localhost'):
+        '''
+        Make connection to OPC server.
+        :param svr_name: string name of OPC server
+        :param host: hostname to search for OPC server, defaults to localhost
+        :returns newly connected OPCclient
+        '''
         opc_client = OpenOPC.open_client(host)
         opc_client.connect(svr_name)
+        dlog.rootlog.info("Connected to OPC server %s on %s", svr_name, host)
         return opc_client
 
     def read(self, PV):
+        '''Reads OPC value from the OPC path given by PV'''
         try:
             return self.client.read(str(PV))
         except OpenOPC.TimeoutError:
-            print 'ERROR!! - OPC timeout waiting for response on ', PV
-            #try again - FOREVER!!!!! (actually until we hit the recursion limit
+            dlog.rootlog.error("OPC timeout waiting for response on %s', PV")
+            #try again - FOREVER!!!!! (actually until we hit the recursion limit)
             return self.read(PV)
 
     def write(self, PV, SP):
+        '''Write value SP to OPC path specified by PV'''
         if type(SP) in [str, unicode]:
             SP = str(SP)
         else:
             SP = float(SP)
-        print self.client.write((str(PV), SP))
+        result = self.client.write((str(PV), SP))
+        dlog.rootlog.debug("OPC write: %s --> %s", PV, SP)
+        return result
 
 
 class OPCdummy(object):
@@ -51,34 +75,39 @@ class OPCdummy(object):
     Dummy class for development without OPC communications
     '''
     def __init__(self, srv_name='OPC.DeltaV.1'):
+        self.server_name = srv_name
         self.client = self.connect_local(srv_name)
+        self.is_dummy = True  # marker to determine if using dummy class at runtime
 
         self.path_dict = dict()  # dictionary of paths for write loopback testing
 
-        self.is_dummy = True
-
-    def connect_local(self, srv_name, host='dummy'):
+    @staticmethod
+    def connect_local(srv_name, host='dummy'):
+        '''Dummy OPC connection'''
+        dlog.rootlog.warning("Connected to dummy OPC client, attempted to get %s on %s", srv_name, host)
         return 'Dummy client connection'
 
     def read(self, path, dummy_val=-99):
-        print "Dummy read: ", path
+        '''Dummy OPC read'''
         if path in self.path_dict:
             dummy_val = self.path_dict[path]
+        dlog.rootlog.debug("Dummy read: %s <-- %s", path, dummy_val)
         return (dummy_val, 'Dummy', time.ctime(time.time()))
 
     def write(self, path, value):
-        print "Dummy write: ", path, ' as ', value
+        dlog.rootlog.debug("Dummy write: %s --> %s", path, value)
         self.path_dict[path] = value
         return 'Success'
 
-# Determine OPC client type from OpenOPC availability
+
+# Determine OPC client type from OpenOPC availability, bind to OPC_Connect name
 if OPC_AVAILABLE:
-    OPC_Connect = OPCconnect
+    OPC_Connect = OPCclient
 else:
     OPC_Connect = OPCdummy
 
 
-# testing
+# trashy testing
 if __name__ == "__main__":
     client = OPC_Connect('OPC.DeltaV.1')
 
